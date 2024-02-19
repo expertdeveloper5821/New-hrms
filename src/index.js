eb2f0020f5815b2876907a52b173fe78b3db8a59
singhthedev <vishal.singh@technogetic.com>
2024-02-19 16:27:54 +0530

    new changes

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const startServer = async () => {
     const server = new ApolloServer({
       typeDefs,
       resolvers,
+      playground: true,
       context: ({ req }) => ({ req }),
     });
 
